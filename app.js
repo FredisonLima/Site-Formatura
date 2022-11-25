a897d15ae165a14927ec5df3bc9073387a1c6634
FredisonLima <105954934+FredisonLima@users.noreply.github.com>
2022-11-25 16:30:28 -0300

    atualizei

---
+++ app.js
@@ -0,0 +1,30 @@
+//Rolagem navegacao
+$('nav a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+
+//slide fotos
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  parent.classList.remove("focus");
+  
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
